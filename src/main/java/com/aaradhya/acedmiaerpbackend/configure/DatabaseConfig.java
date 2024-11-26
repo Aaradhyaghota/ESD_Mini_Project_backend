487481b20a6f611614de4bbcd9ad946bfadc8977
Aaradhyaghota <aaradhyaghota@gmail.com>
2024-11-26 14:59:41 +0530

    added corg and database configuration

---
+++ src/main/java/com/aaradhya/acedmiaerpbackend/configure/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package com.aaradhya.acedmiaerpbackend.configure;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+public class DatabaseConfig {
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/Acedmia_erp");
+//        dataSource.setUsername("aaradhyaghota");
+//        dataSource.setPassword("ubuntu@2911");
+//        return dataSource;
+   }
+//}
