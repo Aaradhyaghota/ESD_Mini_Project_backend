cb765690106817c830ba8af4582d0441f6c00844
Aaradhyaghota <aaradhyaghota@gmail.com>
2024-11-26 15:00:10 +0530

    changes query

--- src/main/java/com/aaradhya/acedmiaerpbackend/repo/StudentRepo.java
+++ src/main/java/com/aaradhya/acedmiaerpbackend/repo/StudentRepo.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface StudentRepo extends JpaRepository<Students, Integer> {
-    @Query("SELECT s.rollNumber FROM Students s WHERE s.domain.domainId = :domainId AND s.graduationYear = :batchYear ORDER BY s.rollNumber DESC LIMIT 1")
+    boolean existsByRollNumber(String rollNumber);
+
+    @Query("SELECT s.rollNumber FROM Students s WHERE s.domain.domainId = :domainId ORDER BY s.rollNumber DESC LIMIT 1")
     String findLastRollNumberByDomainAndBatch(@Param("domainId") int domainId, @Param("batchYear") int batchYear);
 
 }
